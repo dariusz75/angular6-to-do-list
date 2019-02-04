cc055456660b01dc6f590e1877765fee9c747727
Dariusz Franczak <darius32@op.pl>
2019-02-04 23:46:04 +0000

    to do component working

--- src/app/to-do-task/to-do-task.component.ts
+++ src/app/to-do-task/to-do-task.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-to-do-task',
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToDoTaskComponent implements OnInit {
 
+  @Input()
+  tasksList = [];
+  @Output()
+  emitDone = new EventEmitter<string>();
+  @Output()
+  emitRemove = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  remove(task: string) {
+    this.emitRemove.emit(task);
+  }
+
+  done(task: string) {
+    this.emitDone.emit(task);
   }
 
 }
